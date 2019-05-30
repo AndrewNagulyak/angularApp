89a8a5c01851117e3445cf1210071f877825a30b
Andrew Naholik <andriinahyliak58@gmail.com>
2019-05-30 22:23:51 +0300

    first commit

---
+++ Jinder.API/Data/DataDbContext.cs
@@ -0,0 +1,14 @@
+using Jinder.API.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Jinder.API.Data
+{
+    public class DataDbContext:DbContext
+    {
+        public DataDbContext(DbContextOptions<DataDbContext> options) :base (options)
+        {
+            
+        }
+        public DbSet<Value> Values { get; set; }
+    }
+}
---
+++ Jinder.API/Model/Value.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Jinder.API.Model
+{
+    public class Value
+    {
+        public int Id{get;set;}
+        public int Name { get; set; }
+    }
+}
